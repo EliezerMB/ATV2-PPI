887e5f37097a9df3bc75b62a333b15dae11a504c
Eliezer M. Barbosa <eliezerbarbosa832@gmail.com>
2024-11-03 20:54:53 -0300

    Tudo certo por emquanto

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 import express from "express";
+const app = express();
+const porta = 3000;
+const host = "0.0.0.0";
+
+app.listen(porta,host,() =>{
+    console.log('Servidor inicializado e em Execusao no Endereço http://${host}:${porta}')
+});
